87fcf5913584d485d954b85243300491ce459a3a
melaniestnls <83841853+melaniestnls@users.noreply.github.com>
2021-05-08 17:34:39 +0200

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3 
 
-import options, filelist
+import options, filelist, generator
 import message, os, sys
 from sender import *
 
@@ -9,14 +9,24 @@ args = options.arguments()
 
 def receiver():
     os.chdir(dst)
-    fichiersdansdest = filelist.listeFichiersRec(filelist.misajour(os.listdir(os.getcwd())))
+    fichiersdansdest = filelist.listeFichiersSansRec(filelist.misajour(os.listdir(os.getcwd())))
     print(fichiersdansdest,file=sys.stderr )
     fichierssource = []
     (tag,v) = message.receive(0)
     while tag == "fichier":
         fichierssource = fichierssource + [v]
         (tag,v) = message.receive(0)
-
     pid = os.fork()
     if pid == 0 :
         generator.generateur(fichierssource,fichiersdansdest)
+    else:
+        (tag,v)=message.receive(0)
+        while tag != "fin requête":
+            if tag == "debut/créer/écraser":
+                f = os.open(v,O_WRONLY|O_CREAT)
+                (tag,v) = message.receive(0)
+                while tag != "fin":
+                    os.write(f,v)
+                    (tag,v) = message.receive(0)
+            if tag == "créer répertoire":
+                os.mkdir(v)

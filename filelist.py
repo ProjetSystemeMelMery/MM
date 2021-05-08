c6624348ef35f2ddb91113d90b123d21bdd6abbc
melaniestnls <83841853+melaniestnls@users.noreply.github.com>
2021-05-08 13:32:36 +0200

    Update filelist.py

--- filelist.py
+++ filelist.py
@@ -5,31 +5,52 @@
 import os
 import options
 
-args = options.aruguments()
+args = options.arguments()
 
-def listeFichiersSourceSansOp(src):
-    if isinstance(src,list):
-        return src
-    else:
-        if src=='.':
-            src=os.getcwd()
-        if os.path.isfile(src) or os.path.islink(src):
-            return [os.path.basename(src)]
-        if os.path.isdir(src):
-            return os.listdir(src)
+#La liste de fichiers Source contient soit : 
+# - plusieurs fichiers et répertoires (*)
+# - un nom de fichier
+# - un nom de répertoire avec ou sans /
+# - un chemain absolu
+# - un chemin relatif (.) 
+# Pour la suite, il faut être certain de bien connaître le chemin absolu de chaque fichier :
 
-liste = []
-def listeFichiersRec(src):
-    global liste
-    if src=='.':
-        src=os.getcwd()
-    if os.path.isfile(src) or os.path.islink(src):
-        liste = liste + [os.path.basename(src)]
-        return liste
-    if os.path.isdir(src):
-        liste = liste + [os.path.basename(src)]
-        fichiers = os.listdir(src)
-        for e in fichiers:
-            new = os.path.join(src,e)
-            liste.append(listeFichiersRec(new))
+def misajour(src):
+    newsrc = []
+    for e in src:
+        if e == '.':
+            newe = os.getcwd()
+        else :
+            newe = os.path.join(os.getcwd(),e)
+        newsrc.append(newe)
+    return newsrc
+
+
+def listeFichiersSansRec(src):
+    liste = []
+    for e in src:
+        if e[-1]=='/':
+            liste = liste + [(os.path.join(os.getcwd(),e[:-1]),os.path.basename(e[:-1]))]
+        else:
+            liste = liste + [(e,os.path.basename(e))]
+        if e[-1] == '/':
+            if os.path.isdir(e):
+                for elt in os.listdir(e):
+                    liste = liste + [(os.path.join(e,elt),elt)]
     return liste
+
+listerec = []
+def listeFichiersRec(src):
+    global listerec
+    for e in src:
+        if e[-1]=='/':
+            e = e[:-1]
+        if os.path.isfile(e):
+            listerec = listerec + [(e,os.path.basename(e))]
+        if os.path.isdir(e):
+            listerec = listerec + [(e,os.path.basename(e))]
+            l = []
+            for elt in os.listdir(e):
+                l.append(os.path.join(e,elt))
+            listerec.append(listeFichiersRec(l))
+    return listerec
